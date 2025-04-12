3e52f8617a3ae0d98568fb39fb4881fb0662a11f
Joe Rich <joerr1@hotmail.ca>
2025-04-11 21:39:36 -0400

    fix: for real

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,13 +17,11 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: `/${projectName}/`,
-  organizationName,
-  projectName,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'joerr1', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
